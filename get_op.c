89446f725a1a2087d4ccb552369b780d00879ed2
root <priveiro@gmail.com>
2023-05-09 14:38:49 -0700

    agrega a cola 1 vez, y la segunda  no copia el valor de data y leugo segment fault

--- get_op.c
+++ get_op.c
@@ -7,10 +7,11 @@ char *get_op(char *str, instruction_t ops[], stack_t **myStack, unsigned int lin
     char *delim = " \n\0";
     char *opcode;
     stack_t *stack = malloc(sizeof(stack_t));
-    stack_t *findLast;
+    stack_t *findLast =malloc(sizeof(stack_t));
     int data = 0;
 
     token = strtok(str, delim);
+ 
 
     for (i = 0; ops[i].opcode; i++)
     {
@@ -18,25 +19,31 @@ char *get_op(char *str, instruction_t ops[], stack_t **myStack, unsigned int lin
         {
             while (token != NULL)
             {
-                printf("data %s", token);
                 token = strtok(NULL, delim);
+                data = atoi(token);
+                printf("data que pasó por atoi : %d \n", data);
+      
                 if (token != NULL)
                 {
-                    data = atoi(token);
                     stack->n = data;
-                    stack->next = NULL;
+ 
                     if (*myStack == NULL)
                     {
+
                         *myStack = stack;
+                        printf("data copiado en  pila : %d ", stack->n);
+                        break;
                     }
                     else
                     {
                         findLast = *myStack;
                         while (findLast != NULL)
                         {
-                            if (findLast->next == NULL)
+                            if (findLast->next == NULL )
                             {
                                 findLast->next = stack;
+                                printf("data en segunda cola %d\n",findLast->n);
+                                puts("segunda vez");
                                 break;
                             }
                             findLast = findLast->next;
--- push_pall.c
+++ push_pall.c
@@ -34,7 +34,7 @@ int main(int argc, char* argv[])
 	{NULL, NULL}
     };
     
-    my_stack = NULL;
+    *my_stack = NULL;
 
     
 
