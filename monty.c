46b114cace73e4d4a85c11de17e48422b9ca92db
alejandr088 <alejandrivello@gmail.com>
2023-05-12 18:15:59 -0700

    fixed some betty case

--- monty.c
+++ monty.c
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
 	file_ptr = fopen(argv[1], "r");
 	if (file_ptr == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(str, 80, file_ptr) != NULL)
