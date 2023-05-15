7826c7d6c6c0e7a825ad3a26c3f0817f0ead0148
alejandr088 <alejandrivello@gmail.com>
2023-05-14 20:00:07 -0700

    fixed betty

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,6 +1,6 @@
 push 1
 push 2
-pus 2
+push 2
 push 1
 
 push -1024
--- read_file.c
+++ read_file.c
@@ -1,16 +1,16 @@
 #include "monty.h"
+int data = 0;
+
 /**
  * read_file - function to handle files.
  * @file_ptr: pointer to the file to be read
  * @ops: array of operation codes and functions
  * @stack: pointer to top of the stack
 */
-int data = 0;
 
 void read_file(FILE *file_ptr, instruction_t *ops, stack_t **stack)
 {
-	char str[80];
-	char *opcode, *token;
+	char str[80], *opcode, *token;
 	unsigned int lineNum = 1;
 	int opIndex = 0;
 
@@ -34,9 +34,7 @@ void read_file(FILE *file_ptr, instruction_t *ops, stack_t **stack)
 		{
 			data = atoi(token);
 			if (strcmp(token, "0") == 0 && token[0] != '-')
-			{
 				data = 0;
-			}
 			if (data == 0 && strcmp(opcode, "push") == 0)
 			{
 				fprintf(stderr, "L%d: usage: push integer\n", lineNum);
@@ -50,8 +48,6 @@ void read_file(FILE *file_ptr, instruction_t *ops, stack_t **stack)
 		}
 		lineNum++;
 		if (opcode != NULL && opIndex != -1)
-		{
 			ops[opIndex].f(stack, lineNum);
-		}
 	}
 }
